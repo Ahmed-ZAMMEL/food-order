3dfb5ab60ac4b0dac2d7dbdbffbff82cb9ce01b5
Ahmed <ahmed.zammel.info@gmail.com>
2023-08-02 11:10:40 +0200

    Add Ui component, Card

---
+++ src/components/UI/Card.tsx
@@ -0,0 +1,8 @@
+import Props from "../../models/props.model";
+import classes from "./Card.module.scss";
+
+const Card: React.FC<Props> = ({ children }) => (
+  <div className={classes.card}>{children}</div>
+);
+
+export default Card;
