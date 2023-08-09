f3f0fd5be07ab7e1092e1a488dbc7ebbd176139a
Ahmed <ahmed.zammel.info@gmail.com>
2023-08-09 15:04:47 +0200

    Fetch data from server

--- src/components/Meals/AvailableMeals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import Meals from "../../../models/meals.model";
 import Card from "../../UI/Card/Card";
 import classes from "./AvailableMeals.module.scss";
 import MealItem from "./MealItem/MealItem";
+import { error } from "console";
 
 const DUMMY_MEALS: Meals[] = [
   {
@@ -25,9 +27,64 @@ const DUMMY_MEALS: Meals[] = [
 ];
 
 const AvailableMeals: React.FC = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+  const [meals, setMeals] = useState<Meals[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [httpError, setHttpError] = useState<Error>();
+
+  useEffect(() => {
+    //TO DO: replace fetch API withe Axios.
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://food-order-a3596-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        //TO DO create an error class.
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+      const loadedMeals: Meals[] = [];
+      for (const key in responseData) {
+        //TO DO: add type.
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].desciption,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    //TO DO: Use try {} catch {} and await instead of catch().
+    fetchMeals().catch((error) => {
+      let fetchError = error as Error;
+      setIsLoading(false);
+      setHttpError(fetchError);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
+
+  //TO DO: add a general component for loading.
+  if (isLoading) {
+    return (
+      <section className={classes["meals-loading"]}>
+        <p>Loading ...</p>
+      </section>
+    );
+  }
+  //TO DO: add a general
+  if (httpError) {
+    return (
+      <section className={classes["meals-error"]}>
+        <p>{httpError.message}</p>
+      </section>
+    );
+  }
+
   return (
     <section className={classes.meals}>
       <Card>
